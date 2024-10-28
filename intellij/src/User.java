0da98f3fa07b9339e9590a3605fdec734d4a6b29
yliao <yliao@vassar.edu>
2024-10-28 11:14:59 -0400

    user class changes

--- intellij/src/User.java
+++ intellij/src/User.java
@@ -1,14 +1,58 @@
 public class User {
-    public String username;
+    private String username;
+    private Pantry pantry;
 
     public User(String username) {
         this.username = username;
+        this.pantry = new Pantry();
     }
 
+    // Getters and Setters for username
     public void setUsername(String username) {
         this.username = username;
     }
+
     public String getUsername() {
         return username;
     }
+
+    // Getters for Pantry
+    public Pantry getPantry() {
+        return pantry;
+    }
+
+    // Convenience methods to interact with the user's pantry
+//    public void viewPantry() {
+//        System.out.println(pantry);
+//    }
+//
+//    public void addIngredientToPantry(Ingredient ingredient) {
+//        pantry.add_ingredient(ingredient);
+//    }
+//
+//    public void deleteIngredientFromPantry(String name) {
+//        pantry.delete_ingredient(name);
+//    }
+//
+//    public void editIngredientQuantity(String name, int newQuantity) {
+//        pantry.edit_ingredient(name, newQuantity);
+//    }
+//
+//    public void filterPantryByDietaryTag(Ingredient.dietary_tags tag) {
+//        pantry.printIngredientsByTag(tag);
+//    }
+//
+//    public void searchIngredientInPantry(String name) {
+//        pantry.searchIngredient(name);
+//    }
+//
+//    public void addToGroceryList(String name, int quantity) {
+//        pantry.addToGroceryList(name, quantity);
+//    }
+//
+//    public void viewGroceryList() {
+//        pantry.printGroceryList();
+//    }
+//
 }
+
