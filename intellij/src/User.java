9b5d9f9da5bfb71b79d4cca4f5285003b5d9f154
yliao <yliao@vassar.edu>
2024-10-28 11:05:58 -0400

    user class basics

---
+++ intellij/src/User.java
@@ -0,0 +1,14 @@
+public class User {
+    public String username;
+
+    public User(String username) {
+        this.username = username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getUsername() {
+        return username;
+    }
+}
