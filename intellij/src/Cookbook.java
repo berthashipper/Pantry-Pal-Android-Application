dd755a2b32b4b65ad5a5ee5d2f739b0d9e4e7a9e
yliao <yliao@vassar.edu>
2024-10-29 15:55:28 -0400

    cookbook class

---
+++ intellij/src/Cookbook.java
@@ -0,0 +1,34 @@
+import java.util.Set;
+
+public class Cookbook {
+    Set<Recipe> savedRecipes;
+
+    public Cookbook(Set<Recipe> savedRecipes) {
+        this.savedRecipes = savedRecipes;
+    }
+
+    public Set<Recipe> getSavedRecipes() {
+        return savedRecipes;
+    }
+
+    public void deleteAllSavedRecipes() {
+        savedRecipes.clear();
+    }
+
+    public void deleteSavedRecipe(Recipe recipe) {
+        savedRecipes.remove(recipe);
+    }
+
+    public void addSavedRecipe(Recipe recipe) {
+        savedRecipes.add(recipe);
+    }
+
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        for(Recipe r : savedRecipes){
+            sb.append(r.recipeName);
+        }
+        return sb.toString();
+    }
+
+}
